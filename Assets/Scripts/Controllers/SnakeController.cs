39d4bafff9895bcd2f66649074f05d9369668f23
Artem <gpsqaz1@gmail.com>
2024-06-25 15:57:22 +0700

    Created a snake movement

--- Assets/Scripts/Controllers/SnakeController.cs
+++ Assets/Scripts/Controllers/SnakeController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Transform> _tails;
+    [SerializeField] private float _bonesDistance;
+    [SerializeField] private GameObject _bonePrefab;
+
+    [Range(0,4)]
+    [SerializeField] private float _speed;
+
+    private Transform _transform;
+
+    private void Start()
     {
-        
+        _transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveSnake(_transform.position + _transform.forward * _speed);
+
+        float _angel = Input.GetAxis("Horizontal") * 4;
+        _transform.Rotate(0, _angel, 0);
+    }
+
+    private void MoveSnake(Vector3 newPosition)
+    {
+        _transform.position = newPosition;
     }
 }
